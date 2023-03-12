d84b494c9c822f2856eaaa2442303faab4da79c8
Vlada <lartwoods@gmail.com>
2023-03-12 18:04:56 +0300

    added

--- conditions_task2/conditions_script.js
+++ conditions_task2/conditions_script.js
@@ -1,29 +1,15 @@
 function formatDate() {
-  let date = new Date(2024, 1, 10, 11, 23, 23);
-  let dayOfMonth = date.getDate();
-  let month = date.getMonth() + 1;
-  let year = date.getFullYear();
-  let hour = date.getHours();
-  let minutes = date.getMinutes();
-  let diffMs = new Date() - date;
-  let diffSec = Math.round(diffMs / 1000);
-  let diffMin = diffSec / 60;
-  let diffHour = diffMin / 60;
-
-  year = year.toString().slice(-2);
-  month = month < 10 ? "0" + month : month;
-  dayOfMonth = dayOfMonth < 10 ? "0" + dayOfMonth : dayOfMonth;
-  hour = hour < 10 ? "0" + hour : hour;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-
-  if (diffSec < 1) {
+  today = new Date();
+  let nd = new Date(2023, 2, 12, 18, 03);
+  let diff = today - nd;
+  diff = Math.round(diff / 1000);
+  if (diff < 1) {
     console.log("прямо сейчас");
-  } else if (diffMin < 1) {
-    console.log(`${diffSec} сек. назад`);
-  } else if (diffHour < 1) {
-    console.log(`${diffMin} мин. назад`);
+  } else if (diff < 1 * 60) {
+    console.log(`${Math.round(diff)} сек. назад`);
+  } else if (diff < 1 * 60 * 60) {
+    console.log(`${Math.round(diff / 60)} мин. назад`);
   } else {
-    console.log(`${dayOfMonth}.${month}.${year} ${hour}:${minutes}`);
+    console.log(new Date().toLocaleString());
   }
 }
-formatDate();
